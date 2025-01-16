258b84f4aba1f127e7db297c5a5c2efd148af17c
Crystal Kirscht <crystal.kirscht@gmail.com>
2025-01-16 11:52:23 -0600

    committing to not lose changes

--- end2end/tests/lifecycleNewRequest.spec.ts
+++ end2end/tests/lifecycleNewRequest.spec.ts
@@ -3,8 +3,8 @@ import { test, expect } from '@playwright/test';
 test.describe.configure({ mode: 'serial' });
 
 // Generate unique text to help ensure that fields are being filled correctly.
-let randNum = Math.random();
-let uniqueText = `My New Form ${randNum}`;
+// let randNum = Math.random();
+// let uniqueText = `My New Form ${randNum}`;
 
 test('Verify New Request Page', async ({ page }) => {
     await page.goto('https://host.docker.internal/Test_Request_Portal/');
@@ -12,91 +12,58 @@ test('Verify New Request Page', async ({ page }) => {
 
     // Verify Create New Request Page displays
     await expect(page.locator('#record')).toContainText('Step 1 - General Information');
+
 });
 
-test('Create and Submit New Request', async ({ page }) => {
+test('Create and Submit Request', async ({ page }) => {
     await page.goto('https://host.docker.internal/Test_Request_Portal/?a=newform');
-
-    // Fill in Step 1 of Creating a New Request
     await page.getByRole('cell', { name: 'Select an Option Service' }).locator('a').click();
-    await page.getByRole('option', { name: 'Concrete Music' }).click();
+    await page.getByRole('option', { name: 'Bronze Music' }).click();
     await page.getByLabel('Title of Request').click();
-    await page.getByLabel('Title of Request').fill(uniqueText + ' to Create');
-
-    // Choose Simple Form under Step 2 and go to next section
-    await page.locator('label').filter({ hasText: 'Simple form' }).locator('span').click();
+    await page.getByLabel('Title of Request').fill('Request to Create');
+    await page.locator('label').filter({ hasText: 'Multiple person designated' }).locator('span').click();
     await page.getByRole('button', { name: 'Click here to Proceed' }).click();
-
-    // Fill in single line text with 'abc'
-    await expect(page.getByText('1. single line text')).toBeVisible();
-    await page.getByLabel('single line text').click();
-    await page.getByLabel('single line text').fill('abc');
-    await page.locator('#nextQuestion2').click();
-
-    // Verify the name of created request contains the value of uniqueText
-    await expect(page.locator('#requestTitle')).toContainText(uniqueText);
-
-    // Verify the single line text contains 'abc' 
-    await expect(page.locator('#data_11_1')).toContainText('abc');
-
-    // Submit the request
+    await page.getByLabel('Search for user to add as Reviewer 1').click();
+    await page.getByLabel('Search for user to add as Reviewer 1').fill('userName:VTRSHHZOFIA');
+    await page.getByLabel('Search for user to add as Reviewer 2').click();
+    await page.getByLabel('Search for user to add as Reviewer 2').fill('userName:VTRUXEJAMIE');
+    await expect(page.getByTitle('87 - VTRSHHZOFIA')).toContainText('Bauch, Adam Koelpin');
+    await expect(page.getByTitle('25 - VTRUXEJAMIE')).toContainText('Balistreri, Dorian Dickens');
+    await page.locator('#nextQuestion').click();
+    await expect(page.locator('#data_14_1')).toContainText('Adam Bauch');
+    await expect(page.locator('#data_15_1')).toContainText('Dorian Balistreri');
     await page.getByRole('button', { name: 'Submit Request' }).click();
+    await expect(page.locator('#workflowbox_dep-1')).toContainText('Pending action from Adam Bauch');
+})
 
-    // Verify it has been submitted 
-    await expect(page.getByText('Pending Group A')).toBeVisible();    
-});
-
-test('Edit Unsubmitted Request', async ({ page }) => {
-
-    // Go to Create a New Request Page
+test('Edit a New Request', async ({ page }) => {
     await page.goto('https://host.docker.internal/Test_Request_Portal/?a=newform');
-
-    // Fill in the Step 1 options
     await page.getByRole('cell', { name: 'Select an Option Service' }).locator('a').click();
-    await page.getByRole('option', { name: 'Concrete Music' }).click();
+    await page.getByRole('option', { name: 'Concrete Electronics' }).click();
     await page.getByLabel('Title of Request').click();
-
-    // Title the request the value of uniqueTest to Edit
-    await page.getByLabel('Title of Request').fill(uniqueText + ' to Edit');
-
-    // Choose 'Simple Form' as the form to use
+    await page.getByLabel('Title of Request').fill('Request to Edit and Cancel');
     await page.locator('label').filter({ hasText: 'Simple form' }).locator('span').click();
     await page.getByRole('button', { name: 'Click here to Proceed' }).click();
     await page.getByLabel('single line text').click();
-
-    // Fill single line text with 'abc'
-    await page.getByLabel('single line text').fill('abc');
+    await page.getByLabel('single line text').fill('12345');
     await page.locator('#nextQuestion2').click();
-
-    // Click on Edit this Form
+    await expect(page.locator('#data_11_1')).toContainText('12345');
     await page.getByRole('button', { name: 'Edit this form' }).click();
-
-    // Change the single line text to 'Line 1'
-    await page.getByLabel('single line text').click();
-    await page.getByLabel('single line text').fill('Line 1');
+    //await page.getByLabel('single line text').dblclick();
+    await page.getByLabel('single line text').fill('New Text');
+    //await page.locator('#save_indicator').click();
     await page.locator('#nextQuestion2').click();
-
-    // Verify the single line text says 'Line 1'
-    await expect(page.locator('#data_11_1')).toContainText('Line 1');
-});
+    await expect(page.locator('#data_11_1')).toContainText('New Text');
+})
 
 test('Cancel Request', async ({ page }) => {
     await page.goto('https://host.docker.internal/Test_Request_Portal/');
-
-    // Select previous edited request
-    await page.getByRole('link', { name: uniqueText + ' to Edit' }).click();
-
-    // Cancel the request
+    await page.getByRole('link', { name: 'Request to Edit and Cancel' }).click();
     await page.getByRole('button', { name: 'Cancel Request' }).click();
-
-    // Enter comment in cancel popup and confirm cancellation
     await page.getByPlaceholder('Enter Comment').click();
-    await page.getByPlaceholder('Enter Comment').fill('No Longer Needed');
+    await page.getByPlaceholder('Enter Comment').fill('No longer needed');
     await page.getByRole('button', { name: 'Yes' }).click();
-
-    // Verify that the request has been cancelled
     await expect(page.locator('#bodyarea')).toContainText('has been cancelled!');
-});
-
-
-
+    await page.getByRole('link', { name: 'Home' }).click();
+    await expect(page.getByRole('link', { name: 'Request to Edit and Cancel' })).not.toBeVisible();
+})
