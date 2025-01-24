71a0ac62b095fe28eadf2a64aa15abacb1879f89
Crystal Kirscht <crystal.kirscht@gmail.com>
2025-01-24 15:07:50 -0600

    Add test for creating a new request

--- end2end/tests/lifecycleNewRequest.spec.ts
+++ end2end/tests/lifecycleNewRequest.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.describe.configure({ mode: 'serial' });
+test.describe.configure({ mode: 'serial'});
 
 // Generate unique text to help ensure that fields are being filled correctly.
 let randNum = Math.random();
@@ -15,69 +15,101 @@ test('Verify New Request Page', async ({ page }) => {
 
 });
 
-test('Create and Submit Request', async ({ page }) => {
-    await page.goto('https://host.docker.internal/Test_Request_Portal/?a=newform');
-    await page.getByRole('cell', { name: 'Select an Option Service' }).locator('a').click();
-    await page.getByRole('option', { name: 'Bronze Music' }).click();
-    await page.getByLabel('Title of Request').click();
-    await page.getByLabel('Title of Request').fill(uniqueText + ' to Create');
-    await page.locator('label').filter({ hasText: 'Multiple person designated' }).locator('span').click();
-    await page.getByRole('button', { name: 'Click here to Proceed' }).click();
-    await page.getByLabel('Search for user to add as Reviewer 1').click();
-    await page.getByLabel('Search for user to add as Reviewer 1').fill('userName:VTRSHHZOFIA');
-    await page.getByLabel('Search for user to add as Reviewer 2').click();
-    await page.getByLabel('Search for user to add as Reviewer 2').fill('userName:VTRUXEJAMIE');
-    await expect(page.getByTitle('87 - VTRSHHZOFIA')).toContainText('Bauch, Adam Koelpin');
-    await expect(page.getByTitle('25 - VTRUXEJAMIE')).toContainText('Balistreri, Dorian Dickens');
-    await page.locator('#nextQuestion').click();
-    await expect(page.locator('#data_14_1')).toContainText('Adam Bauch');
-    await expect(page.locator('#data_15_1')).toContainText('Dorian Balistreri');
-    await page.getByRole('button', { name: 'Submit Request' }).click();
-    await expect(page.locator('#workflowbox_dep-1')).toContainText('Pending action from Adam Bauch');
-});
-
-test('Archived Question Not Visible to New Request', async ({ page }) => {
+test('Unpublished Form Not Visible to New Request', async ({ page }) => {
     await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=form_vue#/');
     await page.getByRole('link', { name: 'Multiple person designated', exact: true }).click();
-    await page.getByRole('button', { name: 'Edit', exact: true }).click();
-    await page.locator('label').filter({ hasText: 'Archive' }).locator('span').click();
-    await page.getByRole('button', { name: 'Save' }).click();
-    await expect(page.locator('span').filter({ hasText: 'Reviewer 2,'})).not.toBeVisible();
+    await page.getByLabel('Status:').selectOption('-1');
     await page.getByRole('link', { name: 'Home' }).click();
-    await page.getByRole('link', { name: uniqueText + ' to Create' }).click();
-    await expect(page.getByText('Reviewer 1', { exact: true })).toBeVisible();
-    await expect(page.getByText('Reviewer 2', { exact: true })).not.toBeVisible();
-});
-
-test('Value Still Visible in Report After Archiving Question', async ({ page }) => {
-    await page.goto('https://host.docker.internal/Test_Request_Portal/?a=reports&v=3');
-    await page.getByRole('cell', { name: 'Current Status' }).locator('a').click();
-    await page.getByRole('option', { name: 'Title' }).click();
-    await page.getByLabel('text', { exact: true }).click();
-    await page.getByLabel('text', { exact: true }).fill(uniqueText + ' to Create');
-    await page.getByRole('button', { name: 'Next Step' }).click();
-    await page.getByText('Multiple person designated', { exact: true }).click();
-    await page.getByTitle('indicatorID: 14\nReviewer').locator('span').click();
-    await page.getByTitle('indicatorID: 15\nReviewer 2 (Archived)').locator('span').click();
-    await page.getByRole('button', { name: 'Generate Report' }).click();
-    await expect(page.locator('[data-indicator-id="15"]')).toContainText('Dorian Balistreri');
-});
-
-test('Previous Value Still Available to Request After Restoring Question', async ({ page }) => {
+    await page.getByText('New Request', { exact: true }).click();
+    await expect(page.getByText('Multiple person designated')).not.toBeVisible();
+  });
+  
+  test('Hidden Form Not Visible to New Request', async ({ page }) => {
     await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=form_vue#/');
     await page.getByRole('link', { name: 'Multiple person designated', exact: true }).click();
-    await expect(page.getByLabel('Form name (24)')).toHaveValue('Multiple person designated');
-    await page.getByRole('link', { name: 'Restore Fields' }).click();
-    await page.reload();
-    await page.getByRole('button', { name: 'Restore this field' }).click();
-    await expect(page.getByText('Reviewer 2')).not.toBeVisible();
-    await page.getByRole('link', { name: 'Form Browser' }).click();
-    await page.getByRole('link', { name: 'Multiple person designated', exact: true }).click();
-    await expect(page.getByText('Reviewer 2')).toBeVisible();
+    await page.getByLabel('Status:').selectOption('0');
     await page.getByRole('link', { name: 'Home' }).click();
-    await page.getByRole('link', { name: uniqueText + ' to Create' }).click();
-    await expect(page.getByText('Reviewer 2')).toBeVisible();
-    await expect(page.locator('#data_15_1')).toContainText('Dorian Balistreri');
+    await page.getByText('New Request', { exact: true }).click();
+    await expect(page.getByText('Multiple person designated')).not.toBeVisible();
+  });
+  
+  test('Available Form is Visible to New Request', async ({ page }) => {
+    await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=form_vue#/');
+    await page.getByRole('link', { name: 'Multiple person designated', exact: true }).click();
+    await page.getByLabel('Status:').selectOption('1');
+    await page.getByRole('link', { name: 'Home' }).click();
+    await page.getByText('New Request', { exact: true }).click();
+    await expect(page.getByText('Multiple person designated')).toBeVisible();
+  });
+
+  test.describe('Archive and Restore Question',() => {
+
+    test.describe.configure({ retries: 0});
+    
+    test('Create and Submit Request', async ({ page }) => {
+        await page.goto('https://host.docker.internal/Test_Request_Portal/?a=newform');
+        await page.getByRole('cell', { name: 'Select an Option Service' }).locator('a').click();
+        await page.getByRole('option', { name: 'Bronze Music' }).click();
+        await page.getByLabel('Title of Request').click();
+        await page.getByLabel('Title of Request').fill(uniqueText + ' to Create');
+        await page.locator('label').filter({ hasText: 'Multiple person designated' }).locator('span').click();
+        await page.getByRole('button', { name: 'Click here to Proceed' }).click();
+        await page.getByLabel('Search for user to add as Reviewer 1').click();
+        await page.getByLabel('Search for user to add as Reviewer 1').fill('userName:VTRSHHZOFIA');
+        await page.getByLabel('Search for user to add as Reviewer 2').click();
+        await page.getByLabel('Search for user to add as Reviewer 2').fill('userName:VTRUXEJAMIE');
+        await expect(page.getByTitle('87 - VTRSHHZOFIA')).toContainText('Bauch, Adam Koelpin');
+        await expect(page.getByTitle('25 - VTRUXEJAMIE')).toContainText('Balistreri, Dorian Dickens');
+        await page.locator('#nextQuestion').click();
+        await expect(page.locator('#data_14_1')).toContainText('Adam Bauch');
+        await expect(page.locator('#data_15_1')).toContainText('Dorian Balistreri');
+        await page.getByRole('button', { name: 'Submit Request' }).click();
+        await expect(page.locator('#workflowbox_dep-1')).toContainText('Pending action from Adam Bauch');
+    });
+
+    test('Archived Question Not Visible to New Request', async ({ page }) => {
+        await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=form_vue#/');
+        await page.getByRole('link', { name: 'Multiple person designated', exact: true }).click();
+        await page.getByRole('button', { name: 'Edit', exact: true }).click();
+        await page.locator('label').filter({ hasText: 'Archive' }).locator('span').click();
+        await page.getByRole('button', { name: 'Save' }).click();
+        await expect(page.locator('span').filter({ hasText: 'Reviewer 2,'})).not.toBeVisible();
+        await page.getByRole('link', { name: 'Home' }).click();
+        await page.getByRole('link', { name: uniqueText + ' to Create' }).click();
+        await expect(page.getByText('Reviewer 1', { exact: true })).toBeVisible();
+        await expect(page.getByText('Reviewer 2', { exact: true })).not.toBeVisible();
+    });
+
+    test('Value Still Visible in Report After Archiving Question', async ({ page }) => {
+        await page.goto('https://host.docker.internal/Test_Request_Portal/?a=reports&v=3');
+        await page.getByRole('cell', { name: 'Current Status' }).locator('a').click();
+        await page.getByRole('option', { name: 'Title' }).click();
+        await page.getByLabel('text', { exact: true }).click();
+        await page.getByLabel('text', { exact: true }).fill(uniqueText + ' to Create');
+        await page.getByRole('button', { name: 'Next Step' }).click();
+        await page.getByText('Multiple person designated', { exact: true }).click();
+        await page.getByTitle('indicatorID: 14\nReviewer').locator('span').click();
+        await page.getByTitle('indicatorID: 15\nReviewer 2 (Archived)').locator('span').click();
+        await page.getByRole('button', { name: 'Generate Report' }).click();
+        await expect(page.locator('[data-indicator-id="15"]')).toContainText('Dorian Balistreri');
+    });
+
+    test('Previous Value Still Available to Request After Restoring Question', async ({ page }) => {
+        await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=form_vue#/');
+        await page.getByRole('link', { name: 'Multiple person designated', exact: true }).click();
+        await expect(page.getByLabel('Form name (24)')).toHaveValue('Multiple person designated');
+        await page.getByRole('link', { name: 'Restore Fields' }).click();
+        await page.reload();
+        await page.getByRole('button', { name: 'Restore this field' }).click();
+        await expect(page.getByText('Reviewer 2')).not.toBeVisible();
+        await page.getByRole('link', { name: 'Form Browser' }).click();
+        await page.getByRole('link', { name: 'Multiple person designated', exact: true }).click();
+        await expect(page.getByText('Reviewer 2')).toBeVisible();
+        await page.getByRole('link', { name: 'Home' }).click();
+        await page.getByRole('link', { name: uniqueText + ' to Create' }).click();
+        await expect(page.getByText('Reviewer 2')).toBeVisible();
+        await expect(page.locator('#data_15_1')).toContainText('Dorian Balistreri');
+    });
 });
 
 test('Edit a New Request', async ({ page }) => {
