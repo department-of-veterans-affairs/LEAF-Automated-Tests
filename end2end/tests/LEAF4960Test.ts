3cffc601ffc82f40e9138ca155b9c33deb247e8d
Crystal Kirscht <crystal.kirscht@gmail.com>
2025-07-29 12:11:31 -0500

    Draft test for LEAF 4960

---
+++ end2end/tests/LEAF4960Test.ts
@@ -0,0 +1,57 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('LEAF 4960',() => {
+
+  test.describe.configure({ retries: 0});
+
+test('Add customization to print_subindicators templates', async ({ page }) => {
+
+  await page.goto('https://host.docker.internal/Test_Request_Portal/index.php?a=printview&recordID=966');
+  await expect(page.locator('#xhrIndicator_20_1')).toHaveText('test');
+  await page.getByRole('link', { name: 'Home' }).click();
+  await page.getByRole('link', { name: 'Admin Panel' }).click();
+  await page.getByRole('button', { name: ' Template Editor Edit HTML' }).click();
+  await page.getByRole('button', { name: 'print_subindicators', exact: true }).click();
+  //await expect(page.locator('#filename')).toBeVisible();
+  await expect(page.locator('#filename')).toHaveText('print_subindicators');
+  await expect(page.getByText('<!--{strip}-->')).toBeVisible();
+  await page.getByText('<!--{strip}-->').click();
+  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  await page.getByLabel('Template Editor coding area.').press('ArrowUp');
+  await page.getByLabel('Template Editor coding area.').fill('AAAAA');
+  await page.getByRole('button', { name: 'Save Changes' }).click();
+  await page.getByRole('button', { name: 'print_subindicators_ajax' }).click();
+  await expect(page.locator('#filename')).toHaveText('print_subindicators_ajax');
+  // await expect(page.getByText('<!--{**}-->')).toBeVisible();
+  // await page.getByText('<!--{**}-->').click();
+  // await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  // await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  // await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  // await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  // await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  // await page.getByLabel('Template Editor coding area.').press('ArrowUp');
+  // await page.getByLabel('Template Editor coding area.').fill('BBBBB');
+  // await page.getByRole('button', { name: 'Save Changes' }).click();
+  // await page.getByRole('link', { name: 'Home' }).click();
+  // await page.getByRole('link', { name: 'TestForm_GetProgressChecking' }).click();
+  // await expect(page.locator('#xhrIndicator_20_1')).toContainText('BBBBB test AAAAA');
+  // await page.getByRole('link', { name: 'Admin Panel' }).click();
+  // await page.getByRole('button', { name: ' Template Editor Edit HTML' }).click();
+  // await page.getByRole('button', { name: 'print_subindicators', exact: true }).click();
+  // await page.getByRole('button', { name: 'Restore Original' }).click();
+  // await page.getByRole('button', { name: 'Yes' }).click();
+  // await page.getByRole('button', { name: 'print_subindicators_ajax' }).click();
+  // await page.getByRole('button', { name: 'Restore Original' }).click();
+  // await page.getByRole('button', { name: 'Yes' }).click();
+  // await page.getByRole('link', { name: 'Home' }).click();
+  // await page.getByRole('link', { name: 'TestForm_GetProgressChecking' }).click();
+  // await expect(page.locator('#xhrIndicator_20_1')).toContainText('test');
+});
+
+});
