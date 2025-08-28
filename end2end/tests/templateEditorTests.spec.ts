2020b5491b3750ecaf97a51a81afbddd8cc7d24d
Carrie Hanscom <CHanscom7@gmail.com>
2025-08-28 15:08:11 -0400

    updates to test templateEditorTests.spec

--- end2end/tests/templateEditorTests.spec.ts
+++ end2end/tests/templateEditorTests.spec.ts
@@ -1,70 +1,118 @@
 import { test, expect } from '@playwright/test';
 
+const restoreTemplate = async (page:any, currentFileName:string) => {
+  await expect(
+    page.locator('button#restore_original'),
+    `'restore_original' button to be visible because the file ${currentFileName} is customized`
+  ).toBeVisible();
 
-  /**
-   * Test for LEAF 4960
-   * Ensure changes to print_subindicators and print_subindicators_ajax templates are applied
-  */
-  test('Add customization to print_subindicators templates', async ({ page }) => {
+  await page.getByRole('button', { name: 'Restore Original' }).click();
 
-    // Confirm customization has not been added
-    await page.goto('https://host.docker.internal/Test_Request_Portal/index.php?a=printview&recordID=966');
-    await expect(page.locator('#xhrIndicator_20_1')).toHaveText('test');
+  let filePromise = page.waitForResponse(res => res.url().includes(`template/_${currentFileName}`) && res.status() === 200);
+  await page.getByRole('button', { name: 'Yes' }).click();
+  await filePromise;
 
-    // Go to Template Editor
-    await page.getByRole('link', { name: 'Home' }).click();
-    await page.getByRole('link', { name: 'Admin Panel' }).click();
-    await page.getByRole('button', { name: ' Template Editor Edit HTML' }).click();
+  await expect(
+    page.locator('button#restore_original'),
+    `'restore_original' button to be hidden because the file ${currentFileName} has been restored to original`
+  ).toBeHidden();
+}
 
-    // Add 'AAAAA' to the beginning of the print_subindicators template
-    await page.getByRole('button', { name: 'print_subindicators', exact: true }).click();
-    await expect(page.locator('#filename')).toHaveText('print_subindicators');
-    await page.reload();
-    await expect(page.getByText('<!--{strip}-->')).toBeVisible();
-    await page.getByText('<!--{strip}-->').click();
-    await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-    await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-    await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-    await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-    await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-    await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-    await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-    await page.getByLabel('Template Editor coding area.').press('ArrowUp');
-    await page.getByLabel('Template Editor coding area.').fill('AAAAA');
-    await page.getByRole('button', { name: 'Save Changes' }).click();
+/**
+ * Test for LEAF 4960
+ * Ensure changes to print_subindicators and print_subindicators_ajax templates are applied
+*/
+test('Add customization to print_subindicators templates', async ({ page }) => {
+  const requestURL = 'https://host.docker.internal/Test_Request_Portal/index.php?a=printview&recordID=966';
+  const templateEditorURL = 'https://host.docker.internal/Test_Request_Portal/admin/?a=mod_templates&file=print_subindicators.tpl';
 
-    // Add 'BBBBB' to the beginning of the print_subindicators_ajax template
-    // Using the URL because Playwright will not click on the 'print_subindicators_ajax' link
-    await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=mod_templates&file=print_subindicators_ajax.tpl');
-    await expect(page.locator('#filename')).toHaveText('print_subindicators_ajax');
-    await expect(page.getByText('<!--{**}-->')).toBeVisible();
-    await page.getByText('<!--{**}-->').click();
-    await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-    await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-    await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-    await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-    await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-    await page.getByLabel('Template Editor coding area.').press('ArrowUp');
-    await page.getByLabel('Template Editor coding area.').fill('BBBBB');
-    await page.getByRole('button', { name: 'Save Changes' }).click();
+  // Confirm customization has not been added
+  await page.goto(requestURL);
+  await expect(page.locator('#xhrIndicator_20_1')).toHaveText('test');
 
-    // Confirm the 'AAAAA' and 'BBBBB' are visible on the request
-    await page.getByRole('link', { name: 'Home' }).click();
-    await page.getByRole('link', { name: 'TestForm_GetProgressChecking' }).click();
-    await expect(page.locator('#xhrIndicator_20_1')).toContainText('BBBBB test AAAAA');
+  //Go to Template Editor.  file Promises are for load/nav, save and restore.
+  let currentFileName:string = "print_subindicators";
+  let filePromise = page.waitForResponse(res => res.url().includes(`template/_${currentFileName}`) && res.status() === 200);
+  await page.goto(templateEditorURL);
+  await filePromise;
 
-    // Restore the templates back to their original states
-    await page.getByRole('link', { name: 'Admin Panel' }).click();
-    await page.getByRole('button', { name: ' Template Editor Edit HTML' }).click();
-    await page.getByRole('button', { name: 'print_subindicators', exact: true }).click();
-    await page.getByRole('button', { name: 'Restore Original' }).click();
-    await page.getByRole('button', { name: 'Yes' }).click();
-    await page.getByRole('button', { name: 'print_subindicators_ajax' }).click();
-    await page.getByRole('button', { name: 'Restore Original' }).click();
-    await page.getByRole('button', { name: 'Yes' }).click();
+  await expect(
+    page.locator('button#restore_original'),
+    `'restore_original' button to be hidden because the file ${currentFileName} is not customized`
+  ).toBeHidden();
 
-    // Confirm the customization has been removed
-    await page.getByRole('link', { name: 'Home' }).click();
-    await page.getByRole('link', { name: 'TestForm_GetProgressChecking' }).click();
-    await expect(page.locator('#xhrIndicator_20_1')).toContainText('test');
-  });
+  await expect(page.locator('#filename')).toHaveText(currentFileName);
+  await expect(page.getByText('<!--{strip}-->')).toBeVisible();
+  await page.getByText('<!--{strip}-->').click();
+  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  await page.getByLabel('Template Editor coding area.').press('ArrowUp');
+  await page.getByLabel('Template Editor coding area.').fill('AAAAA');
+
+  filePromise = page.waitForResponse(res => res.url().includes(`template/_${currentFileName}`) && res.status() === 200);
+  await page.getByRole('button', { name: 'Save Changes' }).click();
+  await filePromise;
+
+  await expect(
+    page.locator('button#restore_original'),
+    `'restore_original' button to be visible because the file ${currentFileName} has been customized`
+  ).toBeVisible();
+
+
+  // Add 'BBBBB' to the beginning of the print_subindicators_ajax template
+  currentFileName = "print_subindicators_ajax";
+  filePromise = page.waitForResponse(res => res.url().includes(`template/_${currentFileName}`) && res.status() === 200);
+  await page.getByRole('button', { name: currentFileName }).click();
+  await filePromise;
+
+  await expect(
+    page.locator('button#restore_original'),
+    `'restore_original' button to be hidden because the file ${currentFileName} is not customized`
+  ).toBeHidden();
+
+  await expect(page.locator('#filename')).toHaveText(currentFileName);
+  await expect(page.getByText('<!--{**}-->')).toBeVisible();
+  await page.getByText('<!--{**}-->').click();
+  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
+  await page.getByLabel('Template Editor coding area.').press('ArrowUp');
+  await page.getByLabel('Template Editor coding area.').fill('BBBBB');
+  await page.getByRole('button', { name: 'Save Changes' }).click();
+
+  await expect(
+    page.locator('button#restore_original'),
+    `'restore_original' button to be visible because the file ${currentFileName} has been customized`
+  ).toBeVisible();
+
+  // Confirm the 'AAAAA' and 'BBBBB' are visible on the request
+  await page.goto(requestURL);
+  await expect(page.locator('#xhrIndicator_20_1')).toContainText('BBBBB test AAAAA');
+
+
+  // Restore the templates back to their original states
+  currentFileName = "print_subindicators";
+  filePromise = page.waitForResponse(res => res.url().includes(`template/_${currentFileName}`) && res.status() === 200);
+  await page.goto(templateEditorURL);
+  await filePromise;
+
+  await restoreTemplate(page, currentFileName);
+
+  currentFileName = "print_subindicators_ajax";
+  filePromise = page.waitForResponse(res => res.url().includes(`template/_${currentFileName}`) && res.status() === 200);
+  await page.getByRole('button', { name: currentFileName }).click();
+  await filePromise;
+
+  await restoreTemplate(page, currentFileName);
+
+  // Confirm the customization has been removed
+  await page.goto(requestURL);
+  await expect(page.locator('#xhrIndicator_20_1')).toContainText('test');
+});
