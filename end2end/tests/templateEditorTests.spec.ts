040dd7af0c4ad5fe737d2cb356102a32b1027e00
Carrie Hanscom <CHanscom7@gmail.com>
2025-08-30 19:03:41 -0400

    remove some unneeded events, improve template editor test extensibility

--- end2end/tests/templateEditorTests.spec.ts
+++ end2end/tests/templateEditorTests.spec.ts
@@ -1,118 +1,143 @@
 import { test, expect } from '@playwright/test';
 
-const restoreTemplate = async (page:any, currentFileName:string) => {
+const selectAndCustomizeTemplate = async (page:any, fileName:string) => {
+  const filePromise = page.waitForResponse(res =>
+    res.url().includes(`template/_${fileName}.tpl`) && res.status() === 200
+  );
+  await page.getByRole('button', { name: fileName, exact: true }).click();
+  await filePromise;
+
   await expect(
     page.locator('button#restore_original'),
-    `'restore_original' button to be visible because the file ${currentFileName} is customized`
+    `'restore_original' button to be hidden because the file ${fileName} is not customized`
+  ).toBeHidden();
+
+  await expect(
+    page.locator('#filename'),
+    `the Template Editor header to have the name ${fileName}`
+  ).toHaveText(fileName);
+
+  await page.getByLabel('Template Editor coding area.').fill(
+    `<div class="custom_${fileName}">${fileName}</div>`
+  );
+
+  const savePromise = page.waitForResponse(res =>
+    res.url().includes(`templateFileHistory/_${fileName}.tpl`) && res.status() === 200
+  );
+  await page.getByRole('button', { name: 'Save Changes' }).click();
+  await savePromise;
+
+  await expect(
+    page.locator('button#restore_original'),
+    `'restore_original' button to be visible because the file ${fileName} has been customized`
+  ).toBeVisible();
+}
+
+const selectAndRestoreTemplate = async (page:any, fileName:string) => {
+  let filePromise = page.waitForResponse(res =>
+    res.url().includes(`template/_${fileName}.tpl`) && res.status() === 200
+  );
+  await page.getByRole('button', { name: fileName, exact: true }).click();
+  await filePromise;
+
+  await expect(
+    page.locator('button#restore_original'),
+    `'restore_original' button to be visible because the file ${fileName} is customized`
   ).toBeVisible();
 
   await page.getByRole('button', { name: 'Restore Original' }).click();
 
-  let filePromise = page.waitForResponse(res => res.url().includes(`template/_${currentFileName}`) && res.status() === 200);
+  filePromise = page.waitForResponse(res =>
+    res.url().includes(`template/_${fileName}.tpl`) && res.status() === 200
+  );
   await page.getByRole('button', { name: 'Yes' }).click();
   await filePromise;
 
   await expect(
     page.locator('button#restore_original'),
-    `'restore_original' button to be hidden because the file ${currentFileName} has been restored to original`
+    `'restore_original' button to be hidden because the file ${fileName} has been restored to original`
   ).toBeHidden();
 }
 
+
 /**
- * Test for LEAF 4960
  * Ensure changes to print_subindicators and print_subindicators_ajax templates are applied
 */
-test('Add customization to print_subindicators templates', async ({ page }) => {
-  const requestURL = 'https://host.docker.internal/Test_Request_Portal/index.php?a=printview&recordID=966';
-  const templateEditorURL = 'https://host.docker.internal/Test_Request_Portal/admin/?a=mod_templates&file=print_subindicators.tpl';
+test('Templates print_subindicators and print_subindicators_ajax can be customized', async ({ page }) => {
+  const requestURL:string = 'https://host.docker.internal/Test_Request_Portal/index.php?a=printview&recordID=966';
+  //nav from Admin Panel
+  const adminPanelURL:string = 'https://host.docker.internal/Test_Request_Portal/admin/';
+  //nav from file URL
+  const templateEditorPrintSubindicatorsURL:string = 'https://host.docker.internal/Test_Request_Portal/admin/?a=mod_templates&file=print_subindicators.tpl';
+  const requestTemplates: Array<string> = [
+    "print_subindicators",
+    "print_subindicators_ajax",
+  ]
 
-  // Confirm customization has not been added
+  // Confirm customizations have not been added
   await page.goto(requestURL);
-  await expect(page.locator('#xhrIndicator_20_1')).toHaveText('test');
+  await page.waitForLoadState('domcontentloaded');
+  for(let i = 0; i < requestTemplates.length; i++) {
+    const templateName = requestTemplates[i];
+    await expect(
+      page.locator('.custom_' + templateName),
+      `custom modification to ${templateName} not to be found in the document`
+    ).toHaveCount(0);
+  }
 
-  //Go to Template Editor.  file Promises are for load/nav, save and restore.
-  let currentFileName:string = "print_subindicators";
-  let filePromise = page.waitForResponse(res => res.url().includes(`template/_${currentFileName}`) && res.status() === 200);
-  await page.goto(templateEditorURL);
+  //Go to Template Editor from Admin Panel
+  await page.goto(adminPanelURL);
+
+  let filePromise = page.waitForResponse(res =>
+    res.url().includes(`template/_`) && res.status() === 200
+  );
+  await page.getByRole('button', { name: ' Template Editor Edit HTML' }).click();
   await filePromise;
 
   await expect(
-    page.locator('button#restore_original'),
-    `'restore_original' button to be hidden because the file ${currentFileName} is not customized`
-  ).toBeHidden();
-
-  await expect(page.locator('#filename')).toHaveText(currentFileName);
-  await expect(page.getByText('<!--{strip}-->')).toBeVisible();
-  await page.getByText('<!--{strip}-->').click();
-  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-  await page.getByLabel('Template Editor coding area.').press('ArrowUp');
-  await page.getByLabel('Template Editor coding area.').fill('AAAAA');
-
-  filePromise = page.waitForResponse(res => res.url().includes(`template/_${currentFileName}`) && res.status() === 200);
-  await page.getByRole('button', { name: 'Save Changes' }).click();
-  await filePromise;
-
-  await expect(
-    page.locator('button#restore_original'),
-    `'restore_original' button to be visible because the file ${currentFileName} has been customized`
+    page.locator('.CodeMirror'),
+    'element with class .CodeMirror to be visible because CodeMirror has mounted'
   ).toBeVisible();
 
+  for(let i = 0; i < requestTemplates.length; i++) {
+    await selectAndCustomizeTemplate(page, requestTemplates[i]);
+  }
 
-  // Add 'BBBBB' to the beginning of the print_subindicators_ajax template
-  currentFileName = "print_subindicators_ajax";
-  filePromise = page.waitForResponse(res => res.url().includes(`template/_${currentFileName}`) && res.status() === 200);
-  await page.getByRole('button', { name: currentFileName }).click();
+  // Confirm customizations are visible on the request
+  await page.goto(requestURL);
+  await page.waitForLoadState('domcontentloaded');
+  for(let i = 0; i < requestTemplates.length; i++) {
+    const templateName = requestTemplates[i];
+    await expect(
+      page.locator('.custom_' + templateName).first(),
+      `custom modification to ${templateName} to be found in the document`
+    ).toContainText(templateName);
+  };
+
+  //Go to Template Editor from template URL, restore templates
+  filePromise = page.waitForResponse(res =>
+    res.url().includes(`template/_`) && res.status() === 200
+  );
+  await page.goto(templateEditorPrintSubindicatorsURL);
   await filePromise;
 
   await expect(
-    page.locator('button#restore_original'),
-    `'restore_original' button to be hidden because the file ${currentFileName} is not customized`
-  ).toBeHidden();
-
-  await expect(page.locator('#filename')).toHaveText(currentFileName);
-  await expect(page.getByText('<!--{**}-->')).toBeVisible();
-  await page.getByText('<!--{**}-->').click();
-  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-  await page.getByLabel('Template Editor coding area.').press('ArrowLeft');
-  await page.getByLabel('Template Editor coding area.').press('ArrowUp');
-  await page.getByLabel('Template Editor coding area.').fill('BBBBB');
-  await page.getByRole('button', { name: 'Save Changes' }).click();
-
-  await expect(
-    page.locator('button#restore_original'),
-    `'restore_original' button to be visible because the file ${currentFileName} has been customized`
+    page.locator('.CodeMirror'),
+    'element with class .CodeMirror to be visible because CodeMirror has mounted'
   ).toBeVisible();
 
-  // Confirm the 'AAAAA' and 'BBBBB' are visible on the request
-  await page.goto(requestURL);
-  await expect(page.locator('#xhrIndicator_20_1')).toContainText('BBBBB test AAAAA');
-
-
-  // Restore the templates back to their original states
-  currentFileName = "print_subindicators";
-  filePromise = page.waitForResponse(res => res.url().includes(`template/_${currentFileName}`) && res.status() === 200);
-  await page.goto(templateEditorURL);
-  await filePromise;
-
-  await restoreTemplate(page, currentFileName);
-
-  currentFileName = "print_subindicators_ajax";
-  filePromise = page.waitForResponse(res => res.url().includes(`template/_${currentFileName}`) && res.status() === 200);
-  await page.getByRole('button', { name: currentFileName }).click();
-  await filePromise;
-
-  await restoreTemplate(page, currentFileName);
+  for(let i = 0; i < requestTemplates.length; i++) {
+    await selectAndRestoreTemplate(page, requestTemplates[i]);
+  }
 
   // Confirm the customization has been removed
   await page.goto(requestURL);
-  await expect(page.locator('#xhrIndicator_20_1')).toContainText('test');
+  await page.waitForLoadState('domcontentloaded');
+  for(let i = 0; i < requestTemplates.length; i++) {
+    const templateName = requestTemplates[i];
+    await expect(
+      page.locator('.custom_' + templateName),
+      `custom modification to ${templateName} not to be found in the document`
+    ).toHaveCount(0);
+  }
 });
