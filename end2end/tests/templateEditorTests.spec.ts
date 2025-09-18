fd0d1507f34b44bc5220535b3d00d6f2682878a8
Crystal Kirscht <crystal.kirscht@gmail.com>
2025-09-18 11:19:15 -0500

    updated Add customization test with changes from button to link

--- end2end/tests/templateEditorTests.spec.ts
+++ end2end/tests/templateEditorTests.spec.ts
@@ -12,12 +12,13 @@ import { test, expect } from '@playwright/test';
     await expect(page.locator('#xhrIndicator_20_1')).toHaveText('test');
 
     // Go to Template Editor
-    await page.getByRole('link', { name: 'Home' }).click();
+    await page.getByRole('link', { name: 'Home'}).click();
     await page.getByRole('link', { name: 'Admin Panel' }).click();
     await page.getByRole('button', { name: ' Template Editor Edit HTML' }).click();
 
+    
     // Add 'AAAAA' to the beginning of the print_subindicators template
-    await page.getByRole('button', { name: 'print_subindicators', exact: true }).click();
+    await page.getByRole('link', { name: 'print_subindicators', exact: true }).click();
     await expect(page.locator('#filename')).toHaveText('print_subindicators');
     await page.reload();
     await expect(page.getByText('<!--{strip}-->')).toBeVisible();
@@ -34,8 +35,7 @@ import { test, expect } from '@playwright/test';
     await page.getByRole('button', { name: 'Save Changes' }).click();
 
     // Add 'BBBBB' to the beginning of the print_subindicators_ajax template
-    // Using the URL because Playwright will not click on the 'print_subindicators_ajax' link
-    await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=mod_templates&file=print_subindicators_ajax.tpl');
+    await page.getByRole('link', { name: 'print_subindicators_ajax', exact: true }).click();
     await expect(page.locator('#filename')).toHaveText('print_subindicators_ajax');
     await expect(page.getByText('<!--{**}-->')).toBeVisible();
     await page.getByText('<!--{**}-->').click();
@@ -49,22 +49,22 @@ import { test, expect } from '@playwright/test';
     await page.getByRole('button', { name: 'Save Changes' }).click();
 
     // Confirm the 'AAAAA' and 'BBBBB' are visible on the request
-    await page.getByRole('link', { name: 'Home' }).click();
+    await page.getByRole('link', { name: 'Home', exact: true }).click();
     await page.getByRole('link', { name: 'TestForm_GetProgressChecking' }).click();
     await expect(page.locator('#xhrIndicator_20_1')).toContainText('BBBBB test AAAAA');
 
     // Restore the templates back to their original states
     await page.getByRole('link', { name: 'Admin Panel' }).click();
     await page.getByRole('button', { name: ' Template Editor Edit HTML' }).click();
-    await page.getByRole('button', { name: 'print_subindicators', exact: true }).click();
+    await page.getByRole('link', { name: 'print_subindicators', exact: true }).click();
     await page.getByRole('button', { name: 'Restore Original' }).click();
     await page.getByRole('button', { name: 'Yes' }).click();
-    await page.getByRole('button', { name: 'print_subindicators_ajax' }).click();
+    await page.getByRole('link', { name: 'print_subindicators_ajax', exact: true }).click();
     await page.getByRole('button', { name: 'Restore Original' }).click();
     await page.getByRole('button', { name: 'Yes' }).click();
 
     // Confirm the customization has been removed
-    await page.getByRole('link', { name: 'Home' }).click();
+    await page.getByRole('link', { name: 'Home', exact: true  }).click();
     await page.getByRole('link', { name: 'TestForm_GetProgressChecking' }).click();
     await expect(page.locator('#xhrIndicator_20_1')).toContainText('test');
   });
