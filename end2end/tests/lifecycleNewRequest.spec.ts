e4e248f5d9357994c402147c1603a6755d662837
Carrie Hanscom <CHanscom7@gmail.com>
2025-09-24 09:57:16 -0400

    simplify setup method and remove some redundant comments and expect strings

--- end2end/tests/lifecycleNewRequest.spec.ts
+++ end2end/tests/lifecycleNewRequest.spec.ts
@@ -30,13 +30,24 @@ const uniqueText = `My New Request ${randNum}`;
  * @param page
  */
 const lifecycleNewRequestSetup = async (page:Page) => {
-  await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=form_vue#/');
+  const addQuestion = async (page:Page, sectionText:string, fillValue:string, format:string) => {
+    const nameInputLabel = sectionText === 'Add Section' ? 'Section Heading' : 'Field Name';
+    await page.getByLabel(sectionText).click();
+    await page.getByLabel(nameInputLabel).fill(fillValue);
+    await page.getByLabel('Input Format').selectOption(format);
+    awaitPromise = page.waitForResponse(res =>
+      res.url().includes('formEditor/newIndicator') &&
+      res.status() === 200
+    );
+    await page.getByRole('button', { name: 'Save' }).click();
+    await awaitPromise;
+  }
 
+  await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=form_vue#/');
   await page.getByRole('button', { name: 'Create Form' }).click();
   await page.getByLabel('Form Name (up to 50').fill(testFormName);
   await page.getByLabel('Form Description (up to 255').fill(testFormDescription);
   await page.getByLabel('Form Name (up to 50').press('Tab');
-
   let awaitPromise = page.waitForResponse(res =>
     res.url().includes('workflow') &&
     res.status() === 200
@@ -44,53 +55,25 @@ const lifecycleNewRequestSetup = async (page:Page) => {
   await page.getByRole('button', { name: 'Save' }).click();
   await awaitPromise;
 
-  await page.getByLabel('Add Section').click();
-  await page.getByLabel('Section Heading').fill(firstIndLabel);
-  await page.getByLabel('Input Format').selectOption('orgchart_employee');
-  awaitPromise = page.waitForResponse(res =>
-    res.url().includes('formEditor/newIndicator') &&
-    res.status() === 200
-  );
-  await page.getByRole('button', { name: 'Save' }).click();
-  await awaitPromise;
-
+  await addQuestion(page, 'Add Section', firstIndLabel, 'orgchart_employee');
   let namePreview = page.locator('.indicator-name-preview', { hasText: firstIndLabel });
   await expect(namePreview).toBeVisible();
   let elID = await namePreview.getAttribute('id') ?? '';
   reviewer1_indID = elID.replace('format_label_', '');
 
-  const categoryLoc = page.locator('#edit-properties-panel .form-id');
-  formCategoryID = await categoryLoc.innerText();
-
-  await page.getByLabel('Add Question to Section').click();
-  await page.getByLabel('Field Name').fill(secondIndLabel);
-  await page.getByLabel('Input Format').selectOption('orgchart_employee');
-  awaitPromise = page.waitForResponse(res =>
-    res.url().includes('formEditor/newIndicator') &&
-    res.status() === 200
-  );
-  await page.getByRole('button', { name: 'Save' }).click();
-  await awaitPromise;
-
+  await addQuestion(page, 'Add Question to Section', secondIndLabel, 'orgchart_employee');
   namePreview = page.locator('.indicator-name-preview', { hasText: secondIndLabel });
   await expect(namePreview).toBeVisible();
   elID = await namePreview.getAttribute('id') ?? '';
   reviewer2_indID = elID.replace('format_label_', '');
 
-  await page.getByLabel('Add Question to Section').click();
-  await page.getByLabel('Field Name').fill(thirdIndLabel);
-  await page.getByLabel('Input Format').selectOption('text');
-  awaitPromise = page.waitForResponse(res =>
-    res.url().includes('formEditor/newIndicator') &&
-    res.status() === 200
-  );
-  await page.getByRole('button', { name: 'Save' }).click();
-  await awaitPromise;
-
+  await addQuestion(page, 'Add Question to Section', thirdIndLabel, 'text');
   namePreview = page.locator('.indicator-name-preview', { hasText: thirdIndLabel });
   await expect(namePreview).toBeVisible();
   elID = await namePreview.getAttribute('id') ?? '';
   singleLineText_indID = elID.replace('format_label_', '');
+
+  formCategoryID = await page.locator('#edit-properties-panel .form-id').innerText() ?? '';
 }
 
 test.beforeAll(async ({ browser }) => {
@@ -99,20 +82,13 @@ test.beforeAll(async ({ browser }) => {
 });
 
 
-test('Portal Home Page to New Request page navigation', async () => {
+test('New Request page can be navigated to from the Portal Home Page', async () => {
   await page.goto('https://host.docker.internal/Test_Request_Portal/');
   await page.getByText('New Request', { exact: true }).click();
-  await expect(
-    page.locator('#record'),
-    'that the New Request page can be navigated to from the Portal Home page'
-  ).toContainText('Step 1 - General Information');
+  await expect(page.locator('#record')).toContainText('Step 1 - General Information');
 });
 
-/**
- *  Verify a form with a status of 'Unpublished' is not available
- *  to be selected when creating a new request
- */
-test('an unpublished Form is Not Visible to New Request', async () => {
+test('an unpublished Form without a workflow is Not Visible to New Request', async () => {
   await page.goto(newRequestURL);
   await expect(page.getByRole('heading', { name: 'Step 2 - Select type of' })).toBeVisible();
   await expect(
@@ -121,10 +97,6 @@ test('an unpublished Form is Not Visible to New Request', async () => {
   ).not.toBeVisible();
 });
 
-/**
- *  Verify a form with available status, without a workflow set, is not available
- *  to be selected when creating a new request
- */
 test('a status-available form without a workflow is not Visible to New Request', async () => {
   await page.goto(formEditorURL_prefix + formCategoryID);
   await expect(page.locator('.indicator-name-preview', { hasText: firstIndLabel })).toBeVisible();
@@ -133,12 +105,8 @@ test('a status-available form without a workflow is not Visible to New Request',
 
   await page.goto(newRequestURL);
   await expect(page.getByRole('heading', { name: 'Step 2 - Select type of' })).toBeVisible();
-  await expect(
-    page.getByText(testFormName),
-      'available form without a workflow not to be an option on the New Request view'
-  ).not.toBeVisible();
+  await expect(page.getByText(testFormName)).not.toBeVisible();
 });
-
 
 test('a status-available form with a workflow is Visible to New Request', async () => {
   await page.goto(formEditorURL_prefix + formCategoryID);
@@ -148,16 +116,10 @@ test('a status-available form with a workflow is Visible to New Request', async 
 
   await page.goto(newRequestURL);
   await expect(page.getByRole('heading', { name: 'Step 2 - Select type of' })).toBeVisible();
-  await expect(
-    page.getByText(testFormName),
-    'available form with a workflow to be an option on the New Request view'
-  ).toBeVisible();
+  await expect(page.getByText(testFormName)).toBeVisible();
 });
 
-/**
- *  Verify a new request can be edited before being submitted
- */
-test('a new request can be created and edited before being submitted', async () => {
+test('a new request can be created, and edited before being submitted', async () => {
   await page.goto(newRequestURL);
   const serviceDropdown = page.locator('#service_chosen');
   await expect(serviceDropdown).toBeVisible();
@@ -201,18 +163,15 @@ test('a new request can be created and edited before being submitted', async () 
   await expect(page.locator(`#data_${reviewer1_indID}_1`)).toContainText('Adam Bauch');
   await expect(page.locator(`#data_${reviewer2_indID}_1`)).toContainText('Dorian Balistreri');
 
-  // Submit the request and verify it has been submitted
+  // Submit the request and verify it has been submitted 
   await page.getByRole('button', { name: 'Submit Request' }).click();
   await expect(
     page.locator('#workflowbox_dep-1'),
-    'warning to be displayed because the form does not include the indicator set'
+    'warning to be displayed because the form does not include the indicator set in the workflow'
   ).toContainText('Warning: User not selected for current action (Contact Administrator)');
 });
 
-/**
- *   Verify a request can be copied if its status is 'Available'
- */
-test('Request With Available Form Can Be Copied', async () => {
+test('a request can be copied if its form status is set to Available', async () => {
   await page.goto(requestPrintPrefix + newRequestID);
   await page.getByRole('button', { name: 'Copy Request' }).click();
   await page.getByRole('button', { name: 'Save Change' }).click();
@@ -226,7 +185,7 @@ test('Request With Available Form Can Be Copied', async () => {
   await expect(page.getByRole('link', { name: uniqueText + ' to Edit, Copy, and Cancel' })).toHaveCount(2);
 });
 
-test("Request with Unpublished Form Cannot Be Copied", async () => {
+test('a request can not be copied if its form status is set to Unpublished', async () => {
   await page.goto(formEditorURL_prefix + formCategoryID);
   await expect(page.locator('.indicator-name-preview', { hasText: firstIndLabel })).toBeVisible();
   await page.getByLabel('Status:').selectOption('-1');
@@ -237,21 +196,14 @@ test("Request with Unpublished Form Cannot Be Copied", async () => {
   await expect(page.getByText('Request could not be copied:')).toBeVisible();
 });
 
-/**
- *  Verify a form with a status of 'Hidden' is not available
- *  to be selected when creating a new request 
- */
-test('Hidden Form Not Visible to New Request', async () => {
+test('a status-hidden form with a workflow is not Visible to New Request', async () => {
   await page.goto(formEditorURL_prefix + formCategoryID);
   await expect(page.locator('.indicator-name-preview', { hasText: firstIndLabel })).toBeVisible();
   await page.getByLabel('Status:').selectOption('0');
 
   await page.goto(newRequestURL);
   await expect(page.getByRole('heading', { name: 'Step 2 - Select type of' })).toBeVisible();
-  await expect(
-    page.getByText(testFormName),
-      'hidden form with a workflow not to be an option on the New Request view'
-  ).not.toBeVisible();
+  await expect(page.getByText(testFormName)).not.toBeVisible();
 });
 
 test("Request with Hidden Form Can Be Copied", async () => {
@@ -269,11 +221,11 @@ test("Request with Hidden Form Can Be Copied", async () => {
 
 
 /**
- *    Set of tests which do the following:
- *    1. Create a request with the "Multiple Person Designated" form
- *    2. Add values to both the "Reviewer 1" and "Reviewer 2" fields
- *    3. Archive the "Reviewer 2" field and verify the report still shows the entered value
- *    4. Restore the "Reviewer 2" field and verify the request still has the old value
+ *  Set of tests which do the following:
+ *  Archive the "Reviewer 2" field and verify that
+ *  it is not visible on a request but the data is still visible in reports
+ *  Restore the "Reviewer 2" field and verify that
+ *  it is again visible on a request and still visible in reports
  */
 test.describe('Archive and Restore Question', () => {
   // Subsequent tests will make changes that will cause previous tests 
@@ -298,11 +250,11 @@ test.describe('Archive and Restore Question', () => {
     await page.goto(requestPrintPrefix + newRequestID);
     await expect(
       page.getByText(firstIndLabel, { exact: true }),
-      'active question to be visisble on the request'
+      'active question to be visible on the request'
     ).toBeVisible();
     await expect(
       page.getByText(secondIndLabel, { exact: true }),
-      'archived question not to be visisble on the request'
+      'archived question not to be visible on the request'
     ).not.toBeVisible();
   });
 
@@ -367,11 +319,11 @@ test.describe('Archive and Restore Question', () => {
 
     await expect(
       page.locator(`#data_${reviewer1_indID}_1`),
-      'active question to be visisble on the request'
+      'active question to be visible on the request'
     ).toContainText('Adam Bauch');
     await expect(
       page.locator(`#data_${reviewer2_indID}_1`),
-      'restored question to be visisble on the request'
+      'restored question to be visible on the request'
     ).toContainText('Dorian Balistreri');
   });
 });
@@ -471,7 +423,7 @@ test('Closing pop up window does not cause workflow form fields to disappear', {
   await page.locator('#genericDialogxhr').press('Escape');
   await expect(
     page.locator('#format_label_4').getByText('Multi line text', { exact: true }),
-    'workflow internal form field to still be visisble'
+    'workflow internal form field to still be visible'
   ).toBeVisible();
 });
 
