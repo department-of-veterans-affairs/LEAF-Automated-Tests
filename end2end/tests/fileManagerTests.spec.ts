bf5f841bee2dde9c8ed55d3b5cf9b7f52b17b798
Crystal Kirscht <crystal.kirscht@gmail.com>
2025-10-10 13:38:30 -0500

    Add comments and try/finally

---
+++ end2end/tests/fileManagerTests.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from '@playwright/test';
+
+/**
+ * end2end test for LEAF 5075
+ */
+test('Upload .mjs files to File Manager', async ({ page }) => {
+
+  let fileUploaded = false;
+
+  try {
+
+    // Go to file manager and upload test .mjs file
+    await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=mod_file_manager');
+    await page.getByRole('button', { name: 'Upload File' }).click();
+    await page.getByLabel('Select file to upload').setInputFiles('./LEAF-Automated-Tests/end2end/artifacts/LEAF-5086.mjs');
+
+    // Verify file has been uploaded
+    await expect(page.locator("[id$='_0_file']")).toContainText('../files/LEAF-5086.mjs');
+    fileUploaded = true;
+
+    // Click on file link and verify it opened with the correct content
+    const filePromise = page.waitForResponse(
+          res => res.url().includes('files/LEAF-5086.mjs') && res.status() === 200
+    );
+      
+    await page.getByRole('link', { name: '../files/LEAF-5086.mjs' }).click();
+    await expect(page.getByText('This is a test file for LEAF 5075 / LEAF 5086')).toBeVisible();
+
+    // Verify that the content type is 'text/javascript'
+    const fileRes = await filePromise;
+    const headerContentType = fileRes.headers()?.['content-type'] ?? '';
+    expect(headerContentType).toBe('text/javascript');
+
+  } finally {
+
+    // if the file was uploaded successfully delete it
+    if(fileUploaded) {
+      await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=mod_file_manager');
+      await page.locator("[id$='_0_delete']").getByRole('link', { name: 'Delete' }).click();
+      await page.getByRole('button', { name: 'Yes' }).click();
+    }
+  }
+});
