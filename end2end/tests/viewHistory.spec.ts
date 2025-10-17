7616fbc3b0d4c95a7c1721205543118dac2c3735
Crystal Kirscht <crystal.kirscht@gmail.com>
2025-10-17 15:14:56 -0500

    everything but cleanup

--- end2end/tests/viewHistory.spec.ts
+++ end2end/tests/viewHistory.spec.ts
@@ -1,22 +1,48 @@
 import { test, expect } from '@playwright/test';
 import {
-    LEAF_URLS, 
     getRandomId,
     createBaseTestWorkflow, 
-    selectChosenDropdownOption
+    createTestForm,
+    addFormQuestion,
+    loadWorkflow
 } from '../leaf_test_utils/leaf_util_methods';
 
 
 const testId = getRandomId();
 
-test('', async ({ page }) => {
+test('Changing Form Field appears in Workflow History', async ({ page }) => {
 
     const workflowName = `workflow_name_${testId}`;
     const stepTitle = "Step 1";
-    const stepElement = page.getByLabel(`workflow step: ${stepTitle}`, { exact: true });
+    const stepElement = await page.getByLabel(`workflow step: ${stepTitle}`, { exact: true });
 
-    const workflowID = createBaseTestWorkflow(page, workflowName, stepTitle);
-    expect(stepElement).toBeVisible();
-    stepElement.click();
+    const workflowAndStepID = await createBaseTestWorkflow(page, workflowName, stepTitle);
+    const workflowID = workflowAndStepID[0];
+    console.log("The workflowID is " + workflowID);
+    const stepID = workflowAndStepID[1];
+    console.log("The stepID is " + stepID);
+    await expect(stepElement).toBeVisible();
+    //await stepElement.click();
 
+    const formID = await createTestForm(page, `form_name_${testId}`, `form_descr_${testId}`);
+    const textSectionID = await addFormQuestion(page, 'Add Section', 'Single line text', 'Single line text');
+    console.log("The textSectionID is " + textSectionID);
+    await page.getByLabel('Workflow: No Workflow. Users').selectOption(workflowID);
+
+    // Go back to the workflow
+    await loadWorkflow(page, workflowID);
+    await expect(stepElement).toBeVisible();
+    //await stepElement.click();
+
+    // Set the Single Line Text form field
+    await stepElement.click();
+    await page.getByLabel('Form Field:').selectOption(textSectionID);
+    await stepElement.click();
+
+    await page.getByRole('button', { name: 'View History' }).click();
+    const historyText = `Tester Tester changed form field to ${textSectionID} in workflow step: ${stepID}`;
+    await expect(page.locator('tbody')).toContainText(historyText);
+    await page.getByRole('button', { name: 'Close' }).click();
+
+    
 });
