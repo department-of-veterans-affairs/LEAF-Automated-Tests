449ecae2ec3a6aa82d151923fd0eb020c1057ef0
Margo Green <mgreen@sierra7.com>
2025-10-23 11:52:12 -0500

    added the check for encoded chars LEAF-5116

--- end2end/tests/adminPanelSettings.spec.ts
+++ end2end/tests/adminPanelSettings.spec.ts
@@ -16,4 +16,28 @@ test('change title', async ({ page }) => {
   await page.getByLabel('Title of LEAF site').fill(uniqueText);
   await page.getByRole('button', { name: 'Save' }).click();
   await expect(page.locator('#headerDescription')).toContainText(uniqueText);
+
+  //LEAF-5116 Site Setting Input Field shows encoded chars
+  //Verify encoded chars does not display
+
+  await page.getByRole('textbox', { name: 'Title of LEAF site' }).press('ControlOrMeta+a');
+  await page.getByRole('textbox', { name: 'Title of LEAF site' }).fill('Leaf & Test');
+  await page.getByRole('textbox', { name: 'Facility Name' }).press('ControlOrMeta+a');
+  await page.getByRole('textbox', { name: 'Facility Name' }).fill('Facility & Name');
+  await page.getByRole('button', { name: 'Save' }).click();
+  // Reload the page
+  await page.reload();
+
+  await expect(page.getByRole('textbox', { name: 'Title of LEAF site' })).toHaveValue('Leaf & Test');
+  await expect(page.getByRole('textbox', { name: 'Facility Name' })).toHaveValue('Facility & Name');
+  await expect(page.locator('#headerDescription')).toContainText('Leaf & Test');
+
+  //Reset the Settings 
+  await page.getByRole('textbox', { name: 'Title of LEAF site' }).press('ControlOrMeta+a');
+  await page.getByRole('textbox', { name: 'Title of LEAF site' }).fill('Leaf Test Site');
+  await page.getByRole('textbox', { name: 'Facility Name' }).press('ControlOrMeta+a');
+  await page.getByRole('textbox', { name: 'Facility Name' }).fill('Standard test database');
+  await page.getByRole('button', { name: 'Save' }).click();
+  // Reload the page
+  await page.reload();
 });
