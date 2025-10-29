228200865f99d03272accdd08032e54eadc9de08
Margo Green <mgreen@sierra7.com>
2025-10-29 09:31:23 -0500

    updated the time stamp

--- end2end/tests/EditorTimestamp.spec.ts
+++ end2end/tests/EditorTimestamp.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect, Page } from '@playwright/test';
 
 
-  test('Verify Template Editor Timestamp', { tag: ['@LEAF-4959'] }, async ({ page }) => {
+  test('Verify Template Editor Timestamp', async ({ page }) => {
 
 
     await page.goto('https://host.docker.internal/Test_Request_Portal/');
@@ -20,25 +20,36 @@ import { test, expect, Page } from '@playwright/test';
     await page.getByRole('button', { name: 'Save Changes' }).click();
     
     const nowTimeStamp = new Date();
-    const currentTimeStamp = nowTimeStamp.toLocaleTimeString();
+    const currentSystemTimeStamp = nowTimeStamp.toLocaleTimeString();
     const dateMainDiv = page.locator('div.file_history_options_date >> div').first();
-    const timeStamped = await dateMainDiv.innerText();
-    console.log(timeStamped);
-    let timeSplit = timeStamped.split("\n", 3 );
-    const currentTimeStamped = timeSplit[1];
-    expect(currentTimeStamped).toBe(currentTimeStamp);
+    const displayedTimeStamped = await dateMainDiv.innerText();
     
+    let displayedTimeSplit = displayedTimeStamped.split("\n", 3 );
+    const currentDisplayedTimeStamped = displayedTimeSplit[1];
+
+    const updatedSytemTimeString = currentSystemTimeStamp.replace(/(AM|PM)/, "").trim();
+    const updatedDisplayTimeString = currentDisplayedTimeStamped.replace(/(AM|PM)/, "").trim();
+
+    const [sysHours, sysMinutes, sysSeconds] = updatedSytemTimeString.split(':').map(Number);
+    const systemTimeStamp = (sysHours * 3600 + sysMinutes * 60 + sysSeconds) * 1000; // Convert to milliseconds
+
+    const [displayHours, displayMinutes, displaySeconds] = updatedDisplayTimeString.split(':').map(Number);
+    const displayedTimeStamp = (displayHours * 3600 + displayMinutes * 60 + displaySeconds) * 1000; // Convert to milliseconds
+  
+    expect(displayedTimeStamp).toBeCloseTo(systemTimeStamp);
+
     //Restore Template
      await page.getByRole('button', { name: 'Restore Original' }).click();
      await page.getByRole('button', { name: 'Yes' }).click();
 
   });
 
- test('Verify Email Template Editor Timestamp', { tag: ['@LEAF-4959'] }, async ({ page }) => {
+ test('Verify Email Template Editor Timestamp',  async ({ page }) => {
 
     
     await page.goto('https://host.docker.internal/Test_Request_Portal/');
-
+    
+    const fileName = 'LEAF_automated_reminder_body';
     await page.getByRole('link', { name: 'Admin Panel' }).click();
     await page.getByRole('button', { name: ' Email Template Editor Add' }).click();
 
@@ -47,15 +58,31 @@ import { test, expect, Page } from '@playwright/test';
     await page.getByRole('textbox', { name: 'Email To:' }).fill('fake@email.com');
     await expect(page.getByRole('button', { name: 'Save Changes' })).toBeVisible();
     await expect(page.locator('#bodyarea')).toContainText('There are no history files.');
-    await page.getByRole('button', { name: 'Save Changes' }).click();  
+   
+   
+    let savePromise = page.waitForResponse(res =>
+    res.url().includes(`templateFileHistory/_${fileName}.tpl`) && res.status() === 200
+    );
+    await page.getByRole('button', { name: 'Save Changes' }).click();
+    await savePromise;
 
     const nowTimeStamp = new Date();
-    const currentTimeStamp = nowTimeStamp.toLocaleTimeString();
+    const currentSystemTimeStamp = nowTimeStamp.toLocaleTimeString();
     const dateMainDiv = page.locator('div.file_history_options_date >> div').first();
-    const timeStamped = await dateMainDiv.innerText();
-    let timeSplit = timeStamped.split("\n", 3 );
-    const currentTimeStamped = timeSplit[1];
-    expect(currentTimeStamped).toBe(currentTimeStamp);
+    const displayedTimeStamped = await dateMainDiv.innerText();
+    let displayedTimeSplit = displayedTimeStamped.split("\n", 3 );
+    const currentDisplayedTimeStamped = displayedTimeSplit[1];
+ 
+    const updatedSytemTimeString = currentSystemTimeStamp.replace(/(AM|PM)/, "").trim();
+    const updatedDisplayTimeString = currentDisplayedTimeStamped.replace(/(AM|PM)/, "").trim();
+
+    const [sysHours, sysMinutes, sysSeconds] = updatedSytemTimeString.split(':').map(Number);
+    const systemTimeStamp = (sysHours * 3600 + sysMinutes * 60 + sysSeconds) * 1000; // Convert to milliseconds
+
+    const [displayHours, displayMinutes, displaySeconds] = updatedDisplayTimeString.split(':').map(Number);
+    const displayedTimeStamp = (displayHours * 3600 + displayMinutes * 60 + displaySeconds) * 1000; // Convert to milliseconds
+  
+    expect(displayedTimeStamp).toBeCloseTo(systemTimeStamp);
 
     //Restore Template
      await page.getByRole('button', { name: 'Restore Original' }).click();
@@ -63,32 +90,52 @@ import { test, expect, Page } from '@playwright/test';
 
     });
 
- test ('Verify Programmer Editor Timestamp', { tag: ['@LEAF-4959'] }, async ({ page }) => {
-      await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=mod_templates_reports&file=example');
+ test ('Verify Programmer Editor Timestamp', async ({ page }) => {
+    await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=mod_templates_reports&file=example');
  
-     await expect(page.getByRole('button', { name: 'New File' })).toBeVisible();
-     await page.getByRole('button', { name: 'New File' }).click();
-     await page.getByRole('textbox', { name: 'Filename:' }).fill('custom_dev_LEAF2');
-     await expect(page.getByRole('button', { name: 'Save' })).toBeVisible();
-     await page.getByRole('button', { name: 'Save' }).click();
-  
+    await expect(page.getByRole('button', { name: 'New File' })).toBeVisible();
+    await page.getByRole('button', { name: 'New File' }).click();
+    await page.getByRole('textbox', { name: 'Filename:' }).fill('custom_dev_LEAF2');
+    await expect(page.getByRole('button', { name: 'Save' })).toBeVisible();
      
-     await page.getByRole('textbox', { name: 'Template Editor coding area.' }).fill('Testing LEAF Programmer ');
-     await expect(page.getByRole('button', { name: 'Save Changes' })).toBeVisible();
-     await page.getByRole('button', { name: 'Save Changes' }).click();
+    const fileName = "custom_dev_LEAF2";
+    let savePromise = page.waitForResponse(res =>
+      res.url().includes(fileName) && res.status() === 200
+    );
+ 
+    const element = page.getByRole('button', { name: 'Save' });
+    await element.waitFor();
+    await element.click();
+    await savePromise;
+
+    const txtBox = page.getByRole('textbox', { name: 'Template Editor coding area.' });
+    txtBox.waitFor();
+    txtBox.fill('Testing LEAF Programmer ');
+    await expect(page.locator('#codeContainer')).toContainText('Testing LEAF Programmer ');
+
+     //await page.getByRole('textbox', { name: 'Template Editor coding area.' }).fill('Testing LEAF Programmer ');
+     //await expect(page.getByRole('button', { name: 'Save Changes' })).toBeVisible();
+    const element2 = page.getByRole('button', { name: 'Save Changes' });
+    await element2.waitFor();
+    await element2.click();
 
     const nowTimeStamp = new Date();
-    const currentTimeStamp = nowTimeStamp.toLocaleTimeString();
-   
-
+    const currentSystemTimeStamp = nowTimeStamp.toLocaleTimeString();
     const dateMainDiv = page.locator('div.file_history_options_date >> div');
-    console.log('System Time1:', dateMainDiv);
-    const timeStamped = await dateMainDiv.innerText();
-    console.log('System Time:',timeStamped);
-    let timeSplit = timeStamped.split("\n", 3 );
-    const currentTimeStamped = timeSplit[1];
+    const displayedTimeStamped = await dateMainDiv.innerText();
+    let displayedTimeSplit = displayedTimeStamped.split("\n", 3 );
+    const currentDisplayedTimeStamped = displayedTimeSplit[1];
     
-    expect(currentTimeStamped).toBe(currentTimeStamp);
+    const updatedSytemTimeString = currentSystemTimeStamp.replace(/(AM|PM)/, "").trim();
+    const updatedDisplayTimeString = currentDisplayedTimeStamped.replace(/(AM|PM)/, "").trim();
+
+    const [sysHours, sysMinutes, sysSeconds] = updatedSytemTimeString.split(':').map(Number);
+    const systemTimeStamp = (sysHours * 3600 + sysMinutes * 60 + sysSeconds) * 1000; // Convert to milliseconds
+
+    const [displayHours, displayMinutes, displaySeconds] = updatedDisplayTimeString.split(':').map(Number);
+    const displayedTimeStamp = (displayHours * 3600 + displayMinutes * 60 + displaySeconds) * 1000; // Convert to milliseconds
+  
+    expect(displayedTimeStamp).toBeCloseTo(systemTimeStamp);
 
     await expect(page.getByRole('button', { name: 'Delete File' })).toBeVisible();
     await page.getByRole('button', { name: 'Delete File' }).click();
@@ -97,7 +144,7 @@ import { test, expect, Page } from '@playwright/test';
 
  });
 
-  test ('Verify File Manager Timestamp', { tag: ['@LEAF-4959'] }, async ({ page }) => {
+  test ('Verify File Manager Timestamp',  async ({ page }) => {
 
     const fileLocationName = '../files/LEAF-5005.txt';
 
