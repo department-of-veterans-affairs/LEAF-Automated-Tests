79325c5a4684b81eb82a49279e4c2cb6e0cc4bba
Crystal Kirscht <crystal.kirscht@gmail.com>
2025-11-04 14:23:47 -0600

    Added test for 5093 to New Requests test file

--- end2end/tests/lifecycleNewRequest.spec.ts
+++ end2end/tests/lifecycleNewRequest.spec.ts
@@ -345,6 +345,65 @@ test('Closing pop up window does not cause workflow form fields to disappear', {
   ).toBeVisible();
 });
 
+
+/**
+ * Test for LEAF 5093 - Required question validation - trigger change events
+ */
+test('Required Not Highlighted When Group is Populated', async ({ page }) => {
+    
+    let requestID = '';
+
+    try{
+        
+        // Create a new request
+        const serviceName = 'Concrete Music';
+        const formName = 'General Form';
+        
+        requestID = await createTestRequest(page, serviceName, uniqueText, formName);
+
+        // Skip first page
+        await page.locator('#nextQuestion2').click();
+
+        // Set assigned person to Hank Healthcare Abbott
+        const assignedUserName = 'vtrjwvangel';
+        const assignedPersonName = 'Abbott, Hank Hessel.';
+        const assignedTitle = '301 - ' + assignedUserName;
+        await page.getByLabel('Search for user to add as Assigned Person', { exact: true }).fill('userName:' + assignedUserName);
+        
+        await expect(page.getByTitle(assignedTitle)).toContainText(assignedPersonName);
+        await page.locator('#nextQuestion2').click();
+
+        // Verify Assigned Group is visible on the 3rd page and click Next Question
+        await expect(page.getByText('Assigned Group', { exact: true })).toBeVisible();
+        await page.locator('#nextQuestion2').click();
+
+        // Verify the Input-Required-Error formatting (white text, red background) is displayed
+        await expect(page.getByText('* Required')).toHaveClass('input-required input-required-error');
+
+        // Set the Assigned Group to "AS Test Group"
+        const assignedGroupID = 'group#200';
+        const assignedGroupName = 'AS Test Group';
+        await page.getByLabel('Search for user to add as Assigned Group').click();
+        await page.getByLabel('Search for user to add as Assigned Group').fill(assignedGroupID);
+        await expect(page.locator('td')).toContainText(assignedGroupName);
+        await page.getByRole('cell', { name: assignedGroupName }).click();
+
+        // Verify the Input-Required-Error formatting is not displayed
+        // (* Request is just red text)
+        await expect(page.getByText('* Required')).toHaveClass('input-required');
+
+    } finally {
+        
+        // delete the request if it was created
+        if(requestID != '') {
+            await page.getByRole('button', { name: 'Cancel Request' }).click();
+            await page.getByRole('button', { name: 'Yes' }).click();
+        }
+    }
+    
+ });
+
+
 //delete created form.
 test.afterAll(async () => {
   await deleteTestFormByFormID(page, formCategoryID);
