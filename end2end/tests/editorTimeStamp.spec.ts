e47d30bcdc5d3e633d5f3157728b2b2aa0a6c64a
Carrie Hanscom <CHanscom7@gmail.com>
2025-11-13 11:39:13 -0500

    LEAF tests 4959 - await files, spacing

--- end2end/tests/editorTimeStamp.spec.ts
+++ end2end/tests/editorTimeStamp.spec.ts
@@ -1,30 +1,27 @@
-import { test, expect, Page } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 
-  test('Verify Template Editor Timestamp', async ({ page }) => {
+test('Verify Template Editor Timestamp', async ({ page }) => {
+    const fileName = 'login.tpl';
+    const awaitFile = page.waitForResponse(res =>
+      res.url().includes(`template/_${fileName}`) && res.status() === 200
+    );
+    await page.goto(`https://host.docker.internal/Test_Request_Portal/admin/?a=mod_templates&file=${fileName}`);
+    await awaitFile;
 
-
-    await page.goto('https://host.docker.internal/Test_Request_Portal/');
-
-    await page.getByRole('link', { name: 'Admin Panel' }).click();
-    await page.getByRole('button', { name: ' Template Editor Edit HTML' }).click();
-
-    await expect(page.getByText('Template Editor Template Tools')).toBeVisible();
-    await page.getByRole('link', { name: 'login' }).click();
-  
     await expect(page.getByText('There are no history files.')).toBeVisible();
 
     await page.locator('div:nth-child(9) > .CodeMirror-line').click();
     await page.getByRole('textbox', { name: 'Template Editor coding area.' }).fill('test');
     await expect(page.getByRole('button', { name: 'Save Changes' })).toBeVisible();
     await page.getByRole('button', { name: 'Save Changes' }).click();
-    
+
     const nowTimeStamp = new Date();
     const currentSystemTimeStamp = nowTimeStamp.toLocaleTimeString();
     const dateMainDiv = page.locator('div.file_history_options_date >> div').first();
     const displayedTimeStamped = await dateMainDiv.innerText();
-    
-    let displayedTimeSplit = displayedTimeStamped.split("\n", 3 );
+
+    const displayedTimeSplit = displayedTimeStamped.split("\n", 3 );
     const currentDisplayedTimeStamped = displayedTimeSplit[1];
 
     const updatedSytemTimeString = currentSystemTimeStamp.replace(/(AM|PM)/, "").trim();
@@ -36,37 +33,34 @@ import { test, expect, Page } from '@playwright/test';
     const [displayHours, displayMinutes, displaySeconds] = updatedDisplayTimeString.split(':').map(Number);
     const displayedTimeStamp = (displayHours * 3600 + displayMinutes * 60 + displaySeconds) * 1000; // Convert to milliseconds
 
-    const toleranceInSeconds = 2; 
+    const toleranceInSeconds = 2;
 
     const timeDiffernce = displayedTimeStamp-systemTimeStamp;
     expect(timeDiffernce).toBeLessThan(toleranceInSeconds *1000);
     expect(timeDiffernce).toBeGreaterThanOrEqual(0);
-   
-  
-   // expect(displayedTimeStamp).toBeCloseTo(systemTimeStamp);
 
     //Restore Template
-     await page.getByRole('button', { name: 'Restore Original' }).click();
-     await page.getByRole('button', { name: 'Yes' }).click();
+    await page.getByRole('button', { name: 'Restore Original' }).click();
+    await page.getByRole('button', { name: 'Yes' }).click();
+});
 
-  });
+test('Verify Email Template Editor Timestamp',  async ({ page }) => {
+    const fileName = 'LEAF_automated_reminder_body.tpl';
+    const eTemplate = 'https://host.docker.internal/Test_Request_Portal/admin/?a=mod_templates_email' +
+      `&file=${fileName}&subjectFile=LEAF_automated_reminder_subject.tpl` +
+      '&emailToFile=LEAF_automated_reminder_emailTo.tpl&emailCcFile=LEAF_automated_reminder_emailCc.tpl';
 
- test('Verify Email Template Editor Timestamp',  async ({ page }) => {
+    const awaitFile = page.waitForResponse(res =>
+      res.url().includes(`emailTemplates/_${fileName}`) && res.status() === 200
+    );
+    await page.goto(eTemplate);
+    await awaitFile;
 
-    
-    await page.goto('https://host.docker.internal/Test_Request_Portal/');
-    
-    const fileName = 'LEAF_automated_reminder_body';
-    await page.getByRole('link', { name: 'Admin Panel' }).click();
-    await page.getByRole('button', { name: ' Email Template Editor Add' }).click();
-
-    await page.getByRole('button', { name: 'Automated Email Reminder' }).click();
-    await page.getByRole('textbox', { name: 'Email To:' }).click();
     await page.getByRole('textbox', { name: 'Email To:' }).fill('fake@email.com');
     await expect(page.getByRole('button', { name: 'Save Changes' })).toBeVisible();
-   
-    let savePromise = page.waitForResponse(res =>
-    res.url().includes(`templateFileHistory/_${fileName}.tpl`) && res.status() === 200
+
+    const savePromise = page.waitForResponse(res =>
+      res.url().includes(`templateFileHistory/_${fileName}`) && res.status() === 200
     );
     await page.getByRole('button', { name: 'Save Changes' }).click();
     await savePromise;
@@ -75,9 +69,9 @@ import { test, expect, Page } from '@playwright/test';
     const currentSystemTimeStamp = nowTimeStamp.toLocaleTimeString();
     const dateMainDiv = page.locator('div.file_history_options_date >> div').first();
     const displayedTimeStamped = await dateMainDiv.innerText();
-    let displayedTimeSplit = displayedTimeStamped.split("\n", 3 );
+    const displayedTimeSplit = displayedTimeStamped.split("\n", 3 );
     const currentDisplayedTimeStamped = displayedTimeSplit[1];
- 
+
     const updatedSytemTimeString = currentSystemTimeStamp.replace(/(AM|PM)/, "").trim();
     const updatedDisplayTimeString = currentDisplayedTimeStamped.replace(/(AM|PM)/, "").trim();
 
@@ -87,34 +81,29 @@ import { test, expect, Page } from '@playwright/test';
     const [displayHours, displayMinutes, displaySeconds] = updatedDisplayTimeString.split(':').map(Number);
     const displayedTimeStamp = (displayHours * 3600 + displayMinutes * 60 + displaySeconds) * 1000; // Convert to milliseconds
 
-    const toleranceInSeconds = 2; 
+    const toleranceInSeconds = 2;
 
     const timeDiffernce = displayedTimeStamp-systemTimeStamp;
     expect(timeDiffernce).toBeLessThan(toleranceInSeconds *1000);
     expect(timeDiffernce).toBeGreaterThanOrEqual(0);
-  
-    //expect(displayedTimeStamp).toBeCloseTo(systemTimeStamp);
 
     //Restore Template
-     await page.getByRole('button', { name: 'Restore Original' }).click();
-     await page.getByRole('button', { name: 'Yes' }).click();
+    await page.getByRole('button', { name: 'Restore Original' }).click();
+    await page.getByRole('button', { name: 'Yes' }).click();
+});
 
-    });
-
- test ('Verify Programmer Editor Timestamp', async ({ page }) => {
+test ('Verify Programmer Editor Timestamp', async ({ page }) => {
     await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=mod_templates_reports&file=example');
-    
+
     const fileName = "custom_dev_LEAF2";
     await expect(page.getByRole('button', { name: 'New File' })).toBeVisible();
     await page.getByRole('button', { name: 'New File' }).click();
     await page.getByRole('textbox', { name: 'Filename:' }).fill(fileName);
     await expect(page.getByRole('button', { name: 'Save' })).toBeVisible();
-     
-    
-    let savePromise = page.waitForResponse(res =>
+
+    const savePromise = page.waitForResponse(res =>
       res.url().includes(fileName) && res.status() === 200
     );
- 
     const element = page.getByRole('button', { name: 'Save' });
     await element.waitFor();
     await element.click();
@@ -125,8 +114,6 @@ import { test, expect, Page } from '@playwright/test';
     txtBox.fill('Testing LEAF Programmer ');
     await expect(page.locator('#codeContainer')).toContainText('Testing LEAF Programmer ');
 
-     //await page.getByRole('textbox', { name: 'Template Editor coding area.' }).fill('Testing LEAF Programmer ');
-     //await expect(page.getByRole('button', { name: 'Save Changes' })).toBeVisible();
     const element2 = page.getByRole('button', { name: 'Save Changes' });
     await element2.waitFor();
     await element2.click();
@@ -137,7 +124,7 @@ import { test, expect, Page } from '@playwright/test';
     const displayedTimeStamped = await dateMainDiv.innerText();
     const displayedTimeSplit = displayedTimeStamped.split("\n", 3 );
     const currentDisplayedTimeStamped = displayedTimeSplit[1];
-    
+
     const updatedSytemTimeString = currentSystemTimeStamp.replace(/(AM|PM)/, "").trim();
     const updatedDisplayTimeString = currentDisplayedTimeStamped.replace(/(AM|PM)/, "").trim();
 
@@ -146,23 +133,20 @@ import { test, expect, Page } from '@playwright/test';
 
     const [displayHours, displayMinutes, displaySeconds] = updatedDisplayTimeString.split(':').map(Number);
     const displayedTimeStamp = (displayHours * 3600 + displayMinutes * 60 + displaySeconds) * 1000; // Convert to milliseconds
-    
-    const toleranceInSeconds = 2; 
-    
+
+    const toleranceInSeconds = 2;
+
     const timeDiffernce = displayedTimeStamp-systemTimeStamp;
     expect(timeDiffernce).toBeLessThan(toleranceInSeconds *1000);
     expect(timeDiffernce).toBeGreaterThanOrEqual(0);
-
-    //expect(displayedTimeStamp).toBeCloseTo(systemTimeStamp);
 
     await expect(page.getByRole('button', { name: 'Delete File' })).toBeVisible();
     await page.getByRole('button', { name: 'Delete File' }).click();
     await expect(page.getByRole('button', { name: 'Yes' })).toBeVisible();
     await page.getByRole('button', { name: 'Yes' }).click();
+});
 
- });
-
-  test ('Verify File Manager Timestamp',  async ({ page }) => {
+test ('Verify File Manager Timestamp',  async ({ page }) => {
 
     const fileLocationName = '../files/LEAF-5005.txt';
 
@@ -172,31 +156,29 @@ import { test, expect, Page } from '@playwright/test';
     await expect(page.getByRole('heading')).toContainText('File Manager');
     await page.getByRole('button', { name: 'Upload File' }).click();
     await page.getByLabel('Select file to upload').setInputFiles(`./artifacts/LEAF-5005.txt`);
-    
+
     const nowDateStamp = new Date();
     const currentDateStamp = nowDateStamp.toLocaleTimeString();
-    console.log("check1", currentDateStamp);
-         
+
+    const awaitFiles = page.waitForResponse(res =>
+      res.url().includes('system/files') && res.status() === 200
+    );
     await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=mod_file_manager');
+    await awaitFiles;
 
     const mainDiv = page.locator('#fileList >> div');
     const rows = mainDiv.locator('table tbody tr');
-    const rowCount =await rows.count();
-    
-
-  for (let i=0; i<rowCount; i++)
-  {
+    const rowCount = await rows.count();
+  for (let i = 0; i < rowCount; i++) {
     const anchorText = await rows.locator('a').nth(i).innerText();
-    
-    if(anchorText?.includes(fileLocationName))
-    {
+    if(anchorText?.includes(fileLocationName)) {
         //Verify Date this test does not include Time
         const cellTime = await rows.nth(i).locator('td:nth-child(2)').innerText();
 
         const displayedTimeSplit = cellTime.split(" ", 2 );
-        
+
         const displayTimeStamp = displayedTimeSplit[1];
-       
+
         const updatedSytemTimeString = currentDateStamp.replace(/(AM|PM)/, "").trim();
 
         const [sysHours, sysMinutes, sysSeconds] = updatedSytemTimeString.split(':').map(Number);
@@ -205,12 +187,12 @@ import { test, expect, Page } from '@playwright/test';
         const [displayHours, displayMinutes, displaySeconds] = displayTimeStamp.split(':').map(Number);
         const displayedTimeStamp = (displayHours * 3600 + displayMinutes * 60 + displaySeconds) * 1000; // Convert to milliseconds
 
-        const toleranceInSeconds = 2; 
+        const toleranceInSeconds = 2;
 
         const timeDiffernce = displayedTimeStamp-systemTimeStamp;
         expect(timeDiffernce).toBeLessThan(toleranceInSeconds *1000);
-        expect(timeDiffernce).toBeGreaterThanOrEqual(0);  
-        
+        expect(timeDiffernce).toBeGreaterThanOrEqual(0);
+
         //Delete File
         const deleteRow = rows.nth(i).locator('td:nth-child(3) a');
         await deleteRow.click();
@@ -220,7 +202,6 @@ import { test, expect, Page } from '@playwright/test';
         ).toBeVisible();
         await page.getByRole('button', { name: 'Yes' }).click();
       }
-  }    
-    await expect(page.getByRole('heading')).toContainText('File Manager');
-
-  });
+  }
+  await expect(page.getByRole('heading')).toContainText('File Manager');
+});
