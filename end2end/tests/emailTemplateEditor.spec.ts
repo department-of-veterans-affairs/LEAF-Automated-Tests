1967e4dab9bc1c9e0846f01dbc982a5d7403ae92
crystalkirscht <34663543+crystalkirscht@users.noreply.github.com>
2025-11-26 11:39:20 -0600

    add try/finally blocks so changes to templates are restored even if the test fails (#209)

--- end2end/tests/emailTemplateEditor.spec.ts
+++ end2end/tests/emailTemplateEditor.spec.ts
@@ -3,56 +3,84 @@ import { test, expect } from '@playwright/test';
 test.describe('editing of email template content', () => {
   test.describe.configure({ mode: 'serial' });
   test('Save Body Contents using Ctrl+S', async ({ page }) => {
-    await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=mod_templates_email&file=CustomEvent_event_valid_edited_body.tpl&name=test+edited+event+description&subjectFile=CustomEvent_event_valid_edited_subject.tpl&emailToFile=CustomEvent_event_valid_edited_emailTo.tpl&emailCcFile=CustomEvent_event_valid_edited_emailCc.tpl');
-    await page.locator('.trumbowyg-editor').fill('');
-    await expect(page.getByRole('button', { name: 'Restore Original' })).not.toBeVisible();
-    await page.locator('.trumbowyg-editor').click();
-    await page.locator('.trumbowyg-editor').fill('Testing 123');
-    await page.keyboard.press('ControlOrMeta+KeyS');
 
-    await expect(page.locator('#codeContainer')).toContainText('Testing 123');
-    await page.getByRole('heading', { name: 'Email Template Editor' }).scrollIntoViewIfNeeded();
-    await expect(page.getByRole('button', { name: 'Restore Original' })).toBeVisible();
-    const awaitRestore = page.waitForResponse(res =>
-      res.status() === 200 && res.request().method() === 'DELETE'
-    );
-    await page.getByRole('button', { name: 'Restore Original' }).click();
-    await page.getByRole('button', { name: 'Yes' }).click();
-    await awaitRestore;
+    let changesSaved = false
+
+    try {
+      await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=mod_templates_email&file=CustomEvent_event_valid_edited_body.tpl&name=test+edited+event+description&subjectFile=CustomEvent_event_valid_edited_subject.tpl&emailToFile=CustomEvent_event_valid_edited_emailTo.tpl&emailCcFile=CustomEvent_event_valid_edited_emailCc.tpl');
+      await page.locator('.trumbowyg-editor').fill('');
+      await expect(page.getByRole('button', { name: 'Restore Original' })).not.toBeVisible();
+      await page.locator('.trumbowyg-editor').click();
+      await page.locator('.trumbowyg-editor').fill('Testing 123');
+      await page.keyboard.press('ControlOrMeta+KeyS');
+      changesSaved = true;
+
+      await expect(page.locator('#codeContainer')).toContainText('Testing 123');
+      await page.getByRole('heading', { name: 'Email Template Editor' }).scrollIntoViewIfNeeded();
+    } finally {
+      if(changesSaved) {
+        await expect(page.getByRole('button', { name: 'Restore Original' })).toBeVisible();
+        const awaitRestore = page.waitForResponse(res =>
+          res.status() === 200 && res.request().method() === 'DELETE'
+        );
+        await page.getByRole('button', { name: 'Restore Original' }).click();
+        await page.getByRole('button', { name: 'Yes' }).click();
+        await awaitRestore;
+      }
+    }
   });
 
   test('Add Variable Using Dropdown', async ({ page }) => {
-    await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=mod_templates_email&file=CustomEvent_event_valid_edited_body.tpl&name=test+edited+event+description&subjectFile=CustomEvent_event_valid_edited_subject.tpl&emailToFile=CustomEvent_event_valid_edited_emailTo.tpl&emailCcFile=CustomEvent_event_valid_edited_emailCc.tpl');
-    await page.locator('.trumbowyg-editor').fill('');
-    await page.getByRole('button', { name: 'Variables' }).click();
-    await page.getByRole('button', { name: 'formType' }).click();
-    await page.getByRole('button', { name: 'Save Changes' }).click();
 
-    await expect(page.locator('#codeContainer')).toContainText('{{$formType}}');
-    await expect(page.getByRole('button', { name: 'Restore Original' })).toBeVisible();
-    const awaitRestore = page.waitForResponse(res =>
-      res.status() === 200 && res.request().method() === 'DELETE'
-    );
-    await page.getByRole('button', { name: 'Restore Original' }).click();
-    await page.getByRole('button', { name: 'Yes' }).click();
-    await awaitRestore;
+    let variableChanged = false;
+
+    try {
+      await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=mod_templates_email&file=CustomEvent_event_valid_edited_body.tpl&name=test+edited+event+description&subjectFile=CustomEvent_event_valid_edited_subject.tpl&emailToFile=CustomEvent_event_valid_edited_emailTo.tpl&emailCcFile=CustomEvent_event_valid_edited_emailCc.tpl');
+      await page.locator('.trumbowyg-editor').fill('');
+      await page.getByRole('button', { name: 'Variables' }).click();
+      await page.getByRole('button', { name: 'formType' }).click();
+      await page.getByRole('button', { name: 'Save Changes' }).click();
+      variableChanged = true;
+
+      await expect(page.locator('#codeContainer')).toContainText('{{$formType}}');
+      await expect(page.getByRole('button', { name: 'Restore Original' })).toBeVisible();
+    } finally {
+      if(variableChanged) {
+        const awaitRestore = page.waitForResponse(res =>
+          res.status() === 200 && res.request().method() === 'DELETE'
+        );
+        await page.getByRole('button', { name: 'Restore Original' }).click();
+        await page.getByRole('button', { name: 'Yes' }).click();
+        await awaitRestore;
+      }
+    }
   });
 
   test('Receive Error Message When Variable Format is Incorrect', async ({ page }) => {
-    await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=mod_templates_email&file=CustomEvent_event_valid_edited_body.tpl&name=test+edited+event+description&subjectFile=CustomEvent_event_valid_edited_subject.tpl&emailToFile=CustomEvent_event_valid_edited_emailTo.tpl&emailCcFile=CustomEvent_event_valid_edited_emailCc.tpl');
-    await page.locator('.trumbowyg-editor').fill('');
-    await page.locator('.trumbowyg-editor').click();
-    await page.locator('.trumbowyg-editor').fill('{{formType}}');
-    await page.getByRole('button', { name: 'Save Changes' }).click();
 
-    await expect(page.getByText('Potential Variable errors in Body: {{formType}} Example: {{$variable}}')).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Restore Original' })).toBeVisible();
-    const awaitRestore = page.waitForResponse(res =>
-      res.status() === 200 && res.request().method() === 'DELETE'
-    );
-    await page.getByRole('button', { name: 'Restore Original' }).click();
-    await page.getByRole('button', { name: 'Yes' }).click();
-    await awaitRestore;
+    let bodyChanged = false;
+
+    try{
+      await page.goto('https://host.docker.internal/Test_Request_Portal/admin/?a=mod_templates_email&file=CustomEvent_event_valid_edited_body.tpl&name=test+edited+event+description&subjectFile=CustomEvent_event_valid_edited_subject.tpl&emailToFile=CustomEvent_event_valid_edited_emailTo.tpl&emailCcFile=CustomEvent_event_valid_edited_emailCc.tpl');
+      await page.locator('.trumbowyg-editor').fill('');
+      await page.locator('.trumbowyg-editor').click();
+      await page.locator('.trumbowyg-editor').fill('{{formType}}');
+      await page.getByRole('button', { name: 'Save Changes' }).click();
+      bodyChanged = true;
+
+      await expect(page.getByText('Potential Variable errors in Body: {{formType}} Example: {{$variable}}')).toBeVisible();
+      await expect(page.getByRole('button', { name: 'Restore Original' })).toBeVisible();
+      
+    } finally {
+      if(bodyChanged) {
+        const awaitRestore = page.waitForResponse(res =>
+        res.status() === 200 && res.request().method() === 'DELETE'
+      );
+        await page.getByRole('button', { name: 'Restore Original' }).click();
+        await page.getByRole('button', { name: 'Yes' }).click();
+        await awaitRestore;
+      }
+    }
   });
 });
 
