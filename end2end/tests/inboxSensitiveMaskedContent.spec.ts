39ebd4fe6c5633050454b736abbafb8993392390
Crystal Kirscht <crystal.kirscht@gmail.com>
2026-01-30 16:00:58 -0600

    changed create form to use util method and changed form and request title to have a unique title

--- end2end/tests/inboxSensitiveMaskedContent.spec.ts
+++ end2end/tests/inboxSensitiveMaskedContent.spec.ts
@@ -1,22 +1,33 @@
 import { test, expect } from '@playwright/test';
-import { getRandomId, LEAF_URLS, createTestRequest } from '../leaf_test_utils/leaf_util_methods.ts';
+import { 
+  getRandomId, 
+  createTestRequest,
+  deleteTestRequestByRequestID,
+  createTestForm,
+  deleteTestFormByFormID,
+  addFormQuestion
+} from '../leaf_test_utils/leaf_util_methods.ts';
 
 test('Verify the Sensitive Content is Masked in Inbox and shown when hovered', async ({ page }) => {
-     // Locators
-  const createFormButton = page.locator("button#createFormButton");
-  const formNameInput = page.locator("input#name");
-  const formDescriptionTextarea = page.locator("textarea#description");
-  const saveButton = page.locator("button#button_save");
-  const savedFormNameInput = page.locator("input#categoryName");
-  const savedFormDescriptionTextarea = page.locator("textarea#categoryDescription");
+
+  let testFormID = '';
+  let testRequestID = '';
+
+  try {
+
+  // Variables
+  const testID = getRandomId();
+  const formTitle = "Masking Form_" + testID;
+  const formDesc = "Sensitive Content Test Description";
+  const sectionTitle = "Section Heading One";
+  const questionText = "This question will have sensitive content";
+  const requestTitle = "Masking Request_" + testID;
+  const userInputAnswer = "This will be hidden";
+  const quickViewBtnXPath = `//a[contains(text(), '${requestTitle}')]/following-sibling::button[contains(text(), 'Quick View')]`
+  
+  // Locators
   const workflowDropdown = page.locator("select#workflowID");
   const statusDropdown = page.locator("select#availability");
-  const addSectionButton = page.locator("div#blank_section_preview button.btn-general");
-  const sectionHeadingTextarea = page.locator("textarea#name");
-  const addQuestionButton = page.locator("button.new_section_question");
-  const questionNameTextarea = page.locator("textarea#name");
-  const formatTypeDropdown = page.locator("select#indicatorType");
-  const sensitiveChkbox = page.locator("label[for='sensitive']");
   const answerInputBox = page.locator("//input[@type='text']");
   const nextQuestionButton = page.locator("button#nextQuestion2");
   const finalSubmitRequestButton = page.locator("div#submitControl button.buttonNorm");
@@ -26,57 +37,23 @@ test('Verify the Sensitive Content is Masked in Inbox and shown when hovered', a
   const inboxHeaderText = page.locator("div#inbox div").first();
   const activeRequestsList = page.locator("div.siteFormContainers button span:first-child");
   const viewRequestBtn = page.locator("div.depContainer button span:last-child");
-  const quickViewBtn = page.locator(`//a[contains(text(), 'Test Request for Sensitive Content')]/following-sibling::button[contains(text(), 'Quick View')]`).nth(0);
+  const quickViewBtn = page.locator(quickViewBtnXPath);
   const maskingContent = page.locator("span.sensitiveIndicator-masked");
   const unmaskedContent = page.locator("span.printResponse");
 
-//   // Variables
-  const formTitle = "Masking_" + getRandomId();
-  const formDesc = "Sensitive Content Test Description";
-  const sectionTitle = "Section Heading One";
-  const questionText = "This question will have sensitive content";
-  const requestTitle = "Test Request for Sensitive Content";
-  const userInputAnswer = "This will be hidden";
+  
 
   
-  // Step 1: Navigate to the form browser
-  await page.goto(LEAF_URLS.FORM_EDITOR);
-  await expect(createFormButton).toBeVisible();
+  testFormID = await createTestForm(page, formTitle, formDesc);
   
-  // Step 2: Create Form
-  await createFormButton.click();
-  await formNameInput.waitFor({ state: "visible" });
-  await formNameInput.fill(formTitle);
-  await formDescriptionTextarea.fill(formDesc);
-  await saveButton.click();
-  await page.waitForResponse((res) =>
-    res.url().includes("/api/formEditor/new") && res.status() === 200
-  );
-  await savedFormNameInput.waitFor({ state: "visible" });
-  
-  // Step 3: Form View: Verify Form Saved Correctly
-  expect(await savedFormNameInput.inputValue()).toBe(formTitle);
-  expect(await savedFormDescriptionTextarea.inputValue()).toBe(formDesc);
-  
-  // Step 4: Panel config: Set Workflow and Status
   await workflowDropdown.selectOption("1");
   await statusDropdown.selectOption("1");
 
-  // Step 5: Add Section with question
-  await addSectionButton.click();
-  await sectionHeadingTextarea.fill(sectionTitle);
-  await saveButton.click();
-
-  await expect(addQuestionButton).toBeVisible();
-  await addQuestionButton.click();
-  await questionNameTextarea.fill(questionText);
-  await formatTypeDropdown.selectOption("text");
-  await sensitiveChkbox.click();
-  await saveButton.click();
-  await expect(page.getByText(questionText)).toBeVisible();
+  await addFormQuestion(page, 'Add Section', sectionTitle);
+  await addFormQuestion(page, 'Add Question to Section', questionText, 'text', '', true);  
 
   // Step 6: Submit a New Request
-  const testRequestID = await createTestRequest(page, 'AS - Service', requestTitle, formTitle);
+  testRequestID = await createTestRequest(page, 'AS - Service', requestTitle, formTitle);
   await expect(page.locator('#headerTab')).toHaveText(`Request #${testRequestID}`);
 
   await answerInputBox.fill(userInputAnswer);
@@ -106,5 +83,18 @@ test('Verify the Sensitive Content is Masked in Inbox and shown when hovered', a
   const rawUnsmaskedContent = await unmaskedContent.textContent();
   const unmaskedContentText = rawUnsmaskedContent ? rawUnsmaskedContent.trim() : '';
   expect(unmaskedContentText).toBe(userInputAnswer);
+
+  } finally {
+    
+    if(testFormID != '') {
+      await deleteTestFormByFormID(page, testFormID);
+    }
+
+    if(testRequestID != '') {
+      await deleteTestRequestByRequestID(page, testRequestID);
+    }
+  }
+
+  
 
 });
